8b0ec49f11af3b9723e964a807af52d6452a88c4
seryga-afk <seryga_94.94@mail.ru>
2022-05-22 18:44:23 +0300

    feat: done 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const { stdin: input, stdout: output } = require('process');
+
+const rl = readline.createInterface({ input, output });
+
+
+const textOut = fs.createWriteStream(path.join(__dirname, 'textOut.txt'));
+
+rl.write('Please enter text!\n');
+
+
+rl.on('line', (text) => {
+    if (text.trim() === 'exit'){
+        rl.close();
+    } 
+  
+    textOut.write(text + "\n");
+});
+
+rl.on('close', () => {
+    process.exit();
+});
+
+process.on('exit', code => {
+if (code === 0) {
+    console.log('Thank you for your attention!');
+} else {
+    console.log(`Something went wrong ${code}`);
+}
+});
