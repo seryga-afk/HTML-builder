38ed747ac05d4bd4d7dedc14c9bc9e63b748844a
seryga-afk <seryga_94.94@mail.ru>
2022-05-22 19:10:37 +0300

    feat: done 03-file-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,26 @@
+const fsPromises = require('fs/promises');
+const path = require('path');
+
+async function fileString() {
+    const files = await fsPromises.readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true });
+
+    for (let i = 0; i < files.length; i++) {
+
+        if (files[i].isFile()) {
+            
+            const pathToFile = path.join(__dirname, 'secret-folder', files[i].name);
+            const stats = await fsPromises.stat(pathToFile);
+    
+    
+            console.log(`${files[i].name.split('.')[0]} - ${files[i].name.split('.')[1]} - ${stats.size}b`)
+        }
+        
+    }
+}
+
+fileString()
+
+
+
+
+
